5fb7a266a1aee11feb41ee0a5ee791aee0f5fb67
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2015-12-06 21:52:52 -0500

    Update lcd_windows.h

--- src/lcd_windows.h
+++ src/lcd_windows.h
@@ -4,4 +4,4 @@
 void set_window(u8 window_number, u8 x, u8 y, u8 width, u8 height);
 void set_window_enable_io(window_enable_t flags_in, window_enable_t flags_out);
 
-#endif
+#endif /* LCD_WINDOW */
